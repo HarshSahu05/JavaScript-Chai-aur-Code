4fa71f1283336541054effbc20935cfe2a8c96bd
Harsh Sahu <144552853+HarshSahu05@users.noreply.github.com>
2024-01-14 05:24:25 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict" // treat all JS code as  newer version
+
+//alert(3+3); // we are using nodejs not browser
+
+//code readability should be high
+
+// null => it is a standalone value in js // it means empty value
+// undefined => declared but not assigned a value
+
+//symbol => unique
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
