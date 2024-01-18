7c00819e12b144fb9f6c1aeb96b9adc84f8e6bf3
Harsh Sahu <144552853+HarshSahu05@users.noreply.github.com>
2024-01-18 15:35:14 +0000

    data types operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -9,14 +9,29 @@
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber); // Nan , 0 in case of null , Nan in case of undefined value to variable
 
-let isLogged = -1;
+// let isLogged = -1;
 
-let booleanLogged = Boolean ( isLogged);
+// let booleanLogged = Boolean ( isLogged);
 
-console.log(typeof booleanLogged);
-console.log(booleanLogged);
+// console.log(typeof booleanLogged);
+// console.log(booleanLogged);
 
 // 1 = true, 0 = true
 // "" = false, " " = true
 // undefined = false , null = false
 // -1 = true
+// ***************operations*****************
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(+true);
+// console.log(-false);
+// console.log(-true);
+// console.log(+false);
+
+let gameCount = 100;
+console.log(++gameCount);
+console.log(gameCount++);
+console.log(gameCount);
