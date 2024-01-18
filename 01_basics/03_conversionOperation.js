f7451337cda2763d88908745bc6f56d533c86191
Harsh Sahu <144552853+HarshSahu05@users.noreply.github.com>
2024-01-18 15:14:53 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+//let score = "33abs";
+//let score = null;
+// let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score)); // as a method
+
+// let valueInNumber = Number (score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // Nan , 0 in case of null , Nan in case of undefined value to variable
+
+let isLogged = -1;
+
+let booleanLogged = Boolean ( isLogged);
+
+console.log(typeof booleanLogged);
+console.log(booleanLogged);
+
+// 1 = true, 0 = true
+// "" = false, " " = true
+// undefined = false , null = false
+// -1 = true
