ebaafecffedf2a1aeb4e41a047c4973a3b250cb8
Harsh Sahu <144552853+HarshSahu05@users.noreply.github.com>
2024-01-24 05:34:28 +0000

    datastypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,4 @@
+// data types are categorized on the basis of how they are store in memory and how to access them
+
+//Primitive data types -- 7 types -- strings, numbers, boolean, null, undefined, symbol, bigint
+// non-primitive or reference type -- arrays, objects, functions, 
