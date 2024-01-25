3af7803d92e275cfe3e48a43a58cbcc3198c85b3
Harsh Sahu <144552853+HarshSahu05@users.noreply.github.com>
2024-01-25 07:29:52 +0000

    maths and num

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,30 @@
+const score = 400;
+// console.log(score);
+const balance = new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+const otherNumber = 1123.898;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000000;
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//++++++++++ Maths +++++++++++
+
+// console.log(Math);
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.random()*10 + 1 );
+// console.log((Math.random()*10) + 1 );
+// console.log(Math.floor((Math.random()*10) + 1));
+
+const max = 20;
+const min = 10;
+console.log(Math.floor(Math.random()*(max-min+1)) + min);
