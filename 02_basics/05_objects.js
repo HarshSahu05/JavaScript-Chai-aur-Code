00d806c5e66593b24cc8e9fd9c2f4348300cf877
Harsh Sahu <144552853+HarshSahu05@users.noreply.github.com>
2024-02-01 04:09:31 +0000

    destructuring object

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,8 @@
+const course = {
+    coursename : "Js in hindi",
+    price : 99,
+    CourseInstructor : "hitesh"
+}
+const {CourseInstructor : instructor} = course; // destructuring
+//console.log(CourseInstructor);
+console.log(instructor);
